fd73ec15c6c321b6c0c09146aeca3197db045baa
thisistia99 <32358554+thisistia99@users.noreply.github.com>
2019-08-21 17:43:18 +0530

    Update lucky.py

--- lucky.py
+++ lucky.py
@@ -9,7 +9,7 @@ res = requests.get('http://google.com/search?q=' + ' '.join(sys.argv[1:]))
 res.raise_for_status()
 
 #Retrieve top search result links
-soup=bs4.BeautifulSoup(res.text, features="lxml")
+soup=bs4.BeautifulSoup(res.text, features="html.parser")
 linkElems = soup.select('div#main > div > div > div > a')
 numOpen = min(5, len(linkElems))
 for i in range(numOpen):
