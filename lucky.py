4f23591da79bff1a3f2730bc7492d13b45a8a964
thisistia99 <32358554+thisistia99@users.noreply.github.com>
2019-08-21 17:40:21 +0530

    Add files via upload

---
+++ lucky.py
@@ -0,0 +1,16 @@
+#! python3
+# lucky.py - Opens several Google search results.
+
+import requests, sys, webbrowser, bs4
+
+
+print('Googling...') # display text while downloading the Google page
+res = requests.get('http://google.com/search?q=' + ' '.join(sys.argv[1:]))
+res.raise_for_status()
+
+#Retrieve top search result links
+soup=bs4.BeautifulSoup(res.text, features="lxml")
+linkElems = soup.select('div#main > div > div > div > a')
+numOpen = min(5, len(linkElems))
+for i in range(numOpen):
+    webbrowser.open('http://google.com' + linkElems[i].get('href'))
